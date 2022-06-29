9bf04bed171da001b3dd5f575ada80adea86f53e
Paolo Morey <paolo.morey@utec.edu.pe>
2022-06-29 02:50:40 -0500

    avance

--- main.cpp
+++ main.cpp
@@ -1,125 +1,24 @@
-#include <bits/stdc++.h>
-#include <omp.h>
-
-#include <vector>
-
-using namespace std;
-
-#define INF 1000
-
-using matrix = vector<vector<double>>;
-
-const int N = 5;
-
-matrix adj = {
-    {INF, 20, 30, 10, 11},
-    {15, INF, 16, 4, 2},
-    {3, 5, INF, 2, 4},
-    {19, 6, 18, INF, 3},
-    {16, 4, 7, 16, INF}};
-
-void printMatrix(matrix &m) {
-    for (auto row : m) {
-        for (auto col : row) {
-            if (col == INF)
-                cout << "INF"
-                     << "\t";
-            else
-                cout << col << "\t";
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-
-double calc_inf(double a, double b, char op = '-') {
-    if (a == INF || b == INF)
-        return INF;
-    if (op == '-')
-        return a - b;
-    return a + b;
-}
-matrix fillInf(int a, int b, matrix m, int start) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (i == a || j == b)
-                m[i][j] = INF;
-        }
-    }
-    m[b][start] = INF;
-    return m;
-}
-
-pair<matrix, double> matrixCost(matrix &m) {
-    double cost = 0, min;
-    matrix newM;
-    for (auto row : m) {
-        min = *min_element(row.begin(), row.end());
-        vector<double> temp;
-        for (auto col : row) {
-            temp.push_back(calc_inf(col, min, '-'));
-        }
-        newM.push_back(temp);
-        cost += min;
-    }
-    printMatrix(newM);
-    for (int j = 0; j < m[0].size(); j++) {
-        min = INF;
-        for (int i = 0; i < m.size(); i++) {
-            if (newM[i][j] < min) {
-                min = newM[i][j];
-            }
-        }
-        cost += min;
-        for (int i = 0; i < newM.size(); i++) {
-            newM[i][j] = calc_inf(newM[i][j], min, '-');
-        }
-    }
-    return {newM, cost};
-}
-
-void TSP(matrix adj, int start = 0) {
-    auto res = matrixCost(adj);
-    auto newM = res.first;
-    auto cost = res.second;
-
-    int path[N + 1];
-    path[0] = start;
-
-    vector<pair<matrix, double>> adjacents;
-
-    for (int i = 0; i < N; i++) {
-        if (i == start || newM[start][i] == INF)
-            continue;
-        auto newAdj = fillInf(start, i, newM, start);
-        auto temp = matrixCost(newAdj);
-        adjacents.push_back({temp.first, cost + temp.second});
-    }
-
-    // TODO: find the node with the lowest cost and call TSP_Recursive on it
-}
-
-void TSP_Recursive(matrix m, int path[], int curr, int level, int currCost) {
-    path[level - 1] = curr;
-    // TODO: Logic for the last level, check for connection to initial node and check if new path is shorter than previous one
-
-    vector<pair<matrix, double>> adjacents;
-
-    for (int i = 0; i < N; i++) {
-        if (i == curr || m[curr][i] == INF)
-            continue;
-        auto newAdj = fillInf(curr, i, m, path[0]);
-        auto temp = matrixCost(newAdj);
-        adjacents.push_back({temp.first, currCost + temp.second});
-    }
-
-    // TODO: find the node with the lowest cost and call TSP_Recursive on it
-}
+#include "tsp.cpp"
 
 int main() {
-    printMatrix(adj);
-    auto res = matrixCost(adj);
-    printMatrix(res.first);
-    cout << res.second << endl;
+    std::vector<std::string> districts = {"Cercado", "Lince", "Miraflores", "Barranco", "Rimac", "Los Olivos",
+                                          "La Molina", "La Victoria", "Magdalena", "San Borja"};
+    matrix adj = {
+            {INF,  5.3,  10,   11.8, 3.5,  12.5, 21.4, 6.5,  6.9,  10.3},
+            {5.3, INF,   4.3,  7.7,  7.7,  17.5, 13.6, 2.8,  5.2,  6},
+            {10,   4.3, INF,   2.9,  13.9, 23.7, 16.6, 8.3,  7.1,  9},
+            {11.8, 7.7,  2.9, INF,   15,   24.8, 17.7, 9.4,  9.8,  10.1},
+            {3.5,  7.7,  13.9, 15, INF,    14.3, 21.5, 9.3,  9.9,  13.1},
+            {12.5, 17.5, 23.7, 24.8, 14.3, INF,  31.7, 18,   19,   27.9},
+            {21.4, 13.6, 16.6, 17.7, 21.5, 31.7, INF,  14.6, 17.3, 9.3},
+            {6.5,  2.8,  8.3,  9.4,  9.3,  18,   14.6, INF,  8.8,  4.7},
+            {6.9,  5.2,  7.1,  9.8,  9.9,  19,   17.3, 8.8, INF,   9.5},
+            {10.3, 6,    9,    10.1, 13.1, 27.9, 9.3,  4.7,  9.5, INF}
+    };
+    TSP a;
+    a.run(adj);
+    auto b = a.get_path();
+    for (int i : b) std::cout<<districts[i]<<"\t";
+    std::cout<<"\nMinimum distance:  "<<a.get_cost();
     return 0;
 }
---
+++ utils.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <ctime>
+
+#define INF 1000
+
+using number = double;
+using matrix = std::vector<std::vector<number>>;
+using vector = std::vector<int>;
+
+struct utils {
+
+    static double calc_inf(double a, double b, char op = '-') {
+        if (a == INF || b == INF) return INF;
+        if (op == '-') return a - b;
+        return a + b;
+    }
+
+    static void print_matrix(const matrix &matrix) {
+        for (const auto &r: matrix) {
+            for (auto c: r) {
+                if (c != INF) std::cout << c << "\t";
+                else std::cout << "inf" << "\t";
+            }
+            std::cout << "\n";
+        }
+        std::cout << "--------------------------\n";
+    }
+
+
+    static void print_vector(const vector &vector) {
+        for (auto e: vector) std::cout << e << "\t";
+        std::cout << "\n";
+    }
+};
+
